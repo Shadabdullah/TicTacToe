6dd37ce627f452ac30829160cd0a37f6b6e04e38
shadAbdullah <shadabdullah101@gmail.com>
2022-08-22 18:08:15 +0530

    Code removed and A dummy file created for dummyCode

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ pygame.init()
 #Constants
 
 WIDTH = 600
-HEIGHT = 600
+HEIGHT = 800
 LINE_WIDTH = 15
 CIRCLE_WIDTH = 15
 CIRCLE_RADIUS= 60
@@ -58,52 +58,13 @@ def draw_figure():
                 pygame.draw.line(screen,XLINE_COLOR,(col* 200 +SPACE,row*200+200-SPACE),(col *200+200-SPACE,row*200+SPACE),CROSS_WIDTH)
                 pygame.draw.line(screen,XLINE_COLOR,(col* 200 +SPACE,row*200+SPACE),(col *200+200-SPACE,row*200+200-SPACE),CROSS_WIDTH)
             
-# Function to restart game
-def restart():
-    screen.fill(RGB_COLOR)
-    for r in range(BORD_ROWS):
-        for c in range(BORD_COLS):
-            board[r][c]=0
+
+
     drawLine() 
-# Tie game
-def tie():
-    screen.fill(RGB_COLOR)
-    pygame.draw.rect()
+
          
 
-def win (player):
-    # Check Horizontaly 
-    # Player 1
-    if (board[0][0]== 1 and board[0][1]==1 and board[0][2]== 1) or (board[0][0]== 2 and board[0][1]==2 and board[0][2]== 2):
-        pygame.draw.line(screen,CIRCLE_COLOR,(0,100),(600,100),LINE_WIDTH)
-        
-    if (board[1][0]==1 and board[1][1]==1 and board[1][2]== 1) or (board[1][0]==2 and board[1][1]==2 and board[1][2]== 2):
-        pygame.draw.line(screen,CIRCLE_COLOR,(0,300),(600,300),LINE_WIDTH)
-        
-    if (board[2][0]==1 and board[2][1]==1 and board[2][2]== 1) or (board[2][0]==2 and board[2][1]==2 and board[2][2]== 2):
-        pygame.draw.line(screen,CIRCLE_COLOR,(0,500),(600,500),LINE_WIDTH)
-    for r in range(BORD_ROWS):
-        board[0][r]== player
-    #Check Vertically
-    if (board[0][0]== 1 and board[1][0]==1 and board[2][0] == 1)or (board[0][0]== 2 and board[1][0]==2 and board[2][0] == 2):
-        pygame.draw.line(screen,CIRCLE_COLOR,(100,0),(100,600),LINE_WIDTH)
-        
-    if (board[0][1]== 1 and board[1][1] ==1 and board[2][1]==1) or (board[0][1]== 2 and board[1][1] ==2 and board[2][1]==2) :
-        pygame.draw.line(screen,CIRCLE_COLOR,(300,0),(300,600),LINE_WIDTH)
-        
 
-    if (board[0][2]==1 and board[1][2]==1 and board[2][2]== 1) or (board[0][2]==2 and board[1][2]==2 and board[2][2]== 2) :
-
-        pygame.draw.line(screen,CIRCLE_COLOR,(500,0),(500,600),LINE_WIDTH)
-       
-    # Check Digonally
-    if (board[0][0]==1 and board[1][1]==1 and board[2][2]==1)or (board[0][0]==2 and board[1][1]==2 and board[2][2]==2):
-        pygame.draw.line(screen,CIRCLE_COLOR,(0,0),(600,600),LINE_WIDTH)
-        
-    if (board[2][0]==1 and board[1][1]==1 and board[0][2]==1)or (board[2][0]==2 and board[1][1]==2 and board[0][2]==2):
-        pygame.draw.line(screen,CIRCLE_COLOR,(0,600),(600,0),LINE_WIDTH)
-       
-    #Change code its not 
 
 #Check Horizontall
 def mark_square(row,col,player):
@@ -138,19 +99,18 @@ while True:
                     if player ==1:
                         mark_square(clicked_row,clicked_col,player)
                         board[clicked_row][clicked_col]=player
-                        win(player)
+                     
                         player = 2
 
                     elif player==2:
                         mark_square(clicked_row,clicked_col,player)
                         board[clicked_row][clicked_col]=player
-                        win(player)
+            
                         player =1
                     
                     draw_figure()
-                    win()
-            else:
-                tie()
+                    
+       
                     
                   
                     
